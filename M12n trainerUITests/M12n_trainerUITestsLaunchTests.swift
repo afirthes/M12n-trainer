8a3334b8539b17e03857404833c679bb3f3e476e
Afir Thes <afirthes@gmail.com>
2021-11-05 15:57:46 +0300

    Initial Commit

---
+++ M12n trainerUITests/M12n_trainerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  M12n_trainerUITestsLaunchTests.swift
+//  M12n trainerUITests
+//
+//  Created by Afir Thes on 05.11.2021.
+//
+
+import XCTest
+
+class M12n_trainerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
