9ed4cf8b1ef69f8ac906fb0832895cbdb36234b5
Afir Thes <afirthes@gmail.com>
2021-11-11 19:08:08 +0300

    More layout

--- M12n trainer/PlayController.swift
+++ M12n trainer/PlayController.swift
@@ -10,9 +10,79 @@ import UIKit
 
 class PlayController: UIViewController {
     
+    @IBOutlet weak var topLeftButton: UIButton!
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    @IBOutlet weak var bottomRightButton: UIButton!
+    
+    private var gradient: CAGradientLayer = CAGradientLayer()
+    @IBOutlet weak var questionTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var closeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gradient.colors = [
+            UIColor(named: "backgroundGradientTop")!.cgColor,
+            //UIColor(named: "backgroundGradientTop")!.cgColor
+            UIColor(named: "backgroundGradientBottom")!.cgColor
+        ]
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width
+        let screenHeight = screenRect.size.height
+        
+        let cornerGap = round((screenWidth - 200) / 3)
+        let middleGap = (screenWidth - 200) - (cornerGap * 2)
+        
+        
+        topLeftButton.translatesAutoresizingMaskIntoConstraints = false
+        topRightButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomLeftButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomRightButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bottomLeftButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: cornerGap),
+            bottomLeftButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -cornerGap),
+            bottomLeftButton.widthAnchor.constraint(equalToConstant: 100),
+            bottomLeftButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            bottomRightButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -cornerGap),
+            bottomRightButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -cornerGap),
+            bottomRightButton.widthAnchor.constraint(equalToConstant: 100),
+            bottomRightButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            topLeftButton.bottomAnchor.constraint(equalTo: bottomLeftButton.topAnchor, constant: -cornerGap),
+            topLeftButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: cornerGap),
+            topLeftButton.widthAnchor.constraint(equalToConstant: 100),
+            topLeftButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            topRightButton.bottomAnchor.constraint(equalTo: bottomRightButton.topAnchor, constant: -cornerGap),
+            topRightButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -cornerGap),
+            topRightButton.widthAnchor.constraint(equalToConstant: 100),
+            topRightButton.heightAnchor.constraint(equalToConstant: 100)
+            
+            
+        ])
+        
+        
+        questionTopConstraint.constant = (screenHeight - cornerGap * 2 - 200) / 2
+        
+        
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.gradient.frame = self.view.bounds
+    }
+    
+    override func viewDidLayoutSubviews() {
         
     }
     
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '15.0'
+
+target 'M12n trainer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  target 'M12n trainerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'M12n trainerUITests' do
+    # Pods for testing
+  end
+
+end
